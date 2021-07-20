e4f46d4b727af8e59208080dbd0cb3c1f76c6e59
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-20 22:06:38 +0000

    Add the pages app url conf for HomePage view

--- urls.py
+++ urls.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url, include
 from aldryn_django.utils import i18n_patterns
 import aldryn_addons.urls
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, include
 
 
 urlpatterns = [
     # add your own patterns here
+
+    path("", include("pages.urls", namespace="pages")),
+
 ] + aldryn_addons.urls.patterns() + i18n_patterns(
     # add your own i18n patterns here
     *aldryn_addons.urls.i18n_patterns()  # MUST be the last entry!
