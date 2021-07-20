c163c98dc926dcc13f08b753678e28fa0e0f1dc6
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-20 22:18:05 +0000

    Register the users app model to the admin interface

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.conf import settings
+from .models import User
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ("username", "email",)
+    list_filter = ("voted",)
+    list_display = ("username", "first_name", "last_name", "email", "voted",)
+    readonly_fields = ("password", "voted",)
+
+
+admin.site.register(User, UserAdmin)
