515c3e37a706263b6a8f25a1d5f380759de7b39c
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-20 22:18:19 +0000

    Add the users app views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.urls.base import reverse_lazy
+from django.contrib.auth.views import LoginView
 
-# Create your views here.
+
+# Login form
+class UserLoginView(LoginView):
+    template_name = "users/login.html"
+    redirect_field_name = reverse_lazy("vote_app:vote-categories")
