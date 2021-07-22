f5b532c298c20c35400caf038c3e9acc947205be
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-22 05:37:12 +0000

    Add url for the electoral page view

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomePage
+from .views import HomePage, ElectoralCommissionBoardPage
 
 app_name="pages"
 urlpatterns = [
     path("", HomePage.as_view(), name="home"),
+    path("electoral-commission-board-members/", ElectoralCommissionBoardPage.as_view(), name="ec-board-page")
 ]
