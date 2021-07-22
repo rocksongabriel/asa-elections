697d449a0e2359038b5c9861f6cc7298c127e947
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-07-22 14:08:02 +0000

    Add method to create user account and email credentials

--- settings.py
+++ settings.py
@@ -85,7 +85,7 @@ LOGOUT_REDIRECT_URL = reverse_lazy("pages:home")
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 587
+    EMAIL_PORT = 587 # ! - if the Can't assign address error comes up, remove this line
     EMAIL_HOST_USER = 'ec.asaelections2021@gmail.com'
     EMAIL_HOST_PASSWORD = 'mbJKKR5kDmzguKs'
     EMAIL_USE_TLS = True
--- users/models.py
+++ users/models.py
@@ -1,42 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
-from django.core.mail import send_mail
-import csv
-from django.conf import settings
 from django.utils.translation import gettext_lazy as _
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib.auth import get_user, get_user_model
 
 
 # Custom User Manager
 class CustomUserManager(UserManager):
     """Custom user manager for the user model"""
 
-    def auto_create_users(self, csv_file):
-        """Create users from a csv file, using the student ID column as the the username, auto generate password for the account and email the credentials to the email associated with the student ID"""
+    def create_user_account_and_send_mail(self, student_id=None, email=None, campus=None):
+        """This method will take a student ID, email, campus and create an account. Then email the credentials to the email"""
+        # generate password
+        password = self.make_random_password(length=8)
+        # create user account
+        self.create_user(username=student_id, email=email, campus=campus, password=password)
+        # send email
+        subject = "Credentials to vote in the ASA Elections 2021"
+        message = f"Student ID: {student_id}\nPassword:{password}\n\nUse it to login so that you can vote."
+        send_mail(
+            subject=subject,
+            message=message, 
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[email],
+            fail_silently=False,
+        )
+        print("Account Created for {student_id} and Email Sent to {email} Successfully")
 
-        if csv_file is None:
-            raise ValueError("A csv file is required")
-
-        # open the csv file
-        with open(csv_file, "r") as file:
-            my_data_reader = csv.reader(file, delimiter=",")
-            students_data = [data for data in my_data_reader][1:]
-
-            # Create accounts and send mail
-            for student_data in students_data:
-                student_id = student_data[0]
-                email = student_data[1]
-                password = self.make_random_password(length=8)
-                self.create_user(student_id, email, password)
-
-                message = f"Student ID: {student_id} -- Password: {password}"
-                send_mail("Credentials for voting",
-                          message,
-                          from_email=settings.DEFAULT_FROM_EMAIL,
-                          recipient_list=[email],
-                          fail_silently=False)
-                print("----------------- Credentials Sent ----------------")
-
-    # TODO - create a method that will take a Student ID, email and create an account and email the credentials to the email
 
 
 class User(AbstractUser):
@@ -58,3 +49,5 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
